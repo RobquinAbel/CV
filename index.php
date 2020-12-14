ab30bcc1c88fdee2d0b6030820fea5ebfe9d52b6
18robquin <abel.robquin1@orange.fr>
2020-12-14 17:28:36 +0100

    Ajout de index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html> 
+<html>
+	<head> 
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>CV</title>
+	</head>
+
+	<body>
+		<header>
+			<h1>Curriculum Vitae</h1>
+		</header>
+
+		<div class="maman">
+
+			<div>
+				<section>
+					<h2>A propos de moi</h2>
+				</section>
+			</div>
+
+		
+			<div class="top">
+				<section class="lightgreyS">
+					<h2>Diplômes</h2>
+				</section>
+
+				<section class="lightgreyM">
+					<h2>Expériences professionelles</h2>
+				</section>
+			</div>
+		</div>
+	</body>
+</html
