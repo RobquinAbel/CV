74b84fc3c1c636960ab2851e76fa3a26563e62a0
RobquinAbel <75306493+RobquinAbel@users.noreply.github.com>
2020-12-16 15:12:10 +0100

    Ajout des pages + barres de nav

---
+++ diplome.php
@@ -0,0 +1,40 @@
+<!doctype html> 
+<html>
+	<head> 
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>CV</title>
+	</head>
+
+	<body>
+		<div class="contain">
+
+			<nav class="navBarPC">
+				<form action="infos.php">
+					<button type="submit" class="nav">
+						<p>A propos de moi</p>
+					</button>
+				</form>
+				<form action="pro.php">
+					<button type="submit" class="nav">
+						<p>Expériences professionelles</p>
+					</button>
+				</form>
+			</nav>
+
+			<header>
+				<h1>Diplômes</h1>
+			</header>
+		</div>
+
+		<nav class="navBarMobile">
+			<a href="infos.php">
+				<button class="navMobile">A propos de moi</button>
+			</a>
+			<a href="pro.php">
+				<button class="navMobile">Expériences professionelles</button>
+			</a>
+		</nav>
+	</body>
+</html>
--- infos.php
+++ infos.php
@@ -13,24 +13,32 @@
 				<h1>Curriculum Vitae</h1>
 			</header>
 
-			<form action="infos.php">
+			<form action="link.php">
 				<button type="submit" class="infos">
 					<h2>A propos de moi</h2>
 				</button>
 			</form>
 
-			<form action="diplome.php">
+			<form action="link.php">
 				<button class="bloc50">
 					<h2>Diplômes</h2>
 				</button>
 			</form>
 
-			<form action="pro.php">
+			<form action="link.php">
 				<button class="bloc50">
 					<h2>Expériences professionelles</h2>
 				</button>
 			</form>
 		</div>
 
+		<nav class="navBarMobile">
+			<a href="infos.php">
+				<button class="navMobile">A propos de moi</button>
+			</a>
+			<a href="pro.php">
+				<button class="navMobile">Expériences professionelles</button>
+			</a>
+		</nav>
 	</body>
 </html>
